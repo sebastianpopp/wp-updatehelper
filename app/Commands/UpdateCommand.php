8e7a7e86f89431218abdefbd2a20c9e3cc784b94
Sebastian Popp <git@sebastianpopp.com>
2023-08-03 18:06:29 +0200

    use laravel prompts

--- app/Commands/UpdateCommand.php
+++ app/Commands/UpdateCommand.php
@@ -8,6 +8,13 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\error;
+use function Laravel\Prompts\multiselect;
+use function Laravel\Prompts\note;
+use function Laravel\Prompts\spin;
+use function Laravel\Prompts\warning;
+
 #[AsCommand(
     name: 'update',
     description: 'Update WordPress and plugins',
@@ -23,31 +30,36 @@ class UpdateCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-
         // Check if working directory is clean
         if (!$this->wdIsClean()) {
-            $io->error("Needs a clean working directory.");
+            error("Needs a clean working directory.");
+
             return Command::FAILURE;
         }
 
         // Open wp-admin
-        if ($io->confirm("Open wp-admin?", true)) {
+        if (confirm("Do you want to open WP admin?")) {
             exec("wp admin");
         }
 
         $changelog = collect([]);
 
         // Check for core updates
-        exec("wp core check-update --format=json", $coreUpdates);
-        $coreUpdates = $coreUpdates ? json_decode($coreUpdates[0], true) : [];
+        $coreUpdates = [];
+        spin(function() use (&$coreUpdates) {
+            exec("wp core check-update --format=json", $coreUpdates);
+            $coreUpdates = $coreUpdates ? json_decode($coreUpdates[0], true) : [];
+        }, "Checking for core updates");
 
         if (sizeof($coreUpdates) > 0) {
             // Get current version
-            exec("wp core version", $oldVersion);
-            $oldVersion = trim($oldVersion[0]);
+            $oldVersion = '';
+            spin(function() use (&$oldVersion) {
+                exec("wp core version", $oldVersion);
+                $oldVersion = trim($oldVersion[0]);
+            }, "Getting current version");
 
-            if ($io->confirm("Update core ($oldVersion → {$coreUpdates[0]['version']})?", true)) {
+            if (confirm("Update core ($oldVersion → {$coreUpdates[0]['version']})?")) {
                 // Update
                 exec("wp core update");
 
@@ -59,7 +71,7 @@ class UpdateCommand extends Command
                 $changelog->push("- Updated WordPress core `{$oldVersion}` → `{$newVersion}`");
 
                 // Git commit
-                if ($io->confirm("Commit to git?", true)) {
+                if (confirm("Commit changes to git?")) {
                     exec("git add -A");
                     exec("git commit -m\"update wp core\"");
                 }
@@ -67,85 +79,104 @@ class UpdateCommand extends Command
         }
 
         // Get list of all plugins
-        exec("wp plugin list --format=json", $plugins);
-        $plugins = json_decode($plugins[0], true);
+        $updateablePlugins = [];
+        spin(function () use (&$updateablePlugins) {
+            exec("wp plugin list --format=json", $plugins);
+            $allPlugins = json_decode($plugins[0], true);
 
-        foreach ($plugins as $plugin) {
-            // Skip plugins that are not active
-            if ($plugin['status'] !== 'active') {
-                $io->info("Skipping {$plugin['name']}");
+
+            // Get list of updateable plugins with information
+            foreach ($allPlugins as $plugin) {
+                // Skip plugins that are not active
+                if ($plugin['status'] !== 'active') {
+                    continue;
+                }
+
+                // Skip plugins that don't have an update available
+                if ($plugin['update'] !== 'available') {
+                    continue;
+                }
+
+                // Get more info
+                $plugininfo = null;
+                exec("wp plugin get {$plugin['name']} --format=json", $plugininfo);
+                $plugininfo = json_decode($plugininfo[0], true);
+
+                $updateablePlugins[$plugin['name']] = $plugininfo['title'];
+            }
+        }, "Getting list of plugins with updates");
+
+        $updatePlugins = multiselect("Select plugins to update", $updateablePlugins, array_keys($updateablePlugins));
+
+        $autoCommit = confirm("Automatically commit changes to git?", true);
+
+        foreach ($updatePlugins as $plugin) {
+            $versions = spin(function () use ($plugin, $updateablePlugins, $autoCommit, &$changelog) {
+                // Update the plugin
+                $pluginupdate = null;
+                exec("wp plugin update {$plugin} --format=json", $pluginupdate);
+                $pluginupdate = json_decode($pluginupdate[0], true);
+
+                // Check for the status
+                if ($pluginupdate[0]['status'] !== "Updated") {
+                    return null;
+                }
+
+                $old_version = $pluginupdate[0]['old_version'];
+                $new_version = $pluginupdate[0]['new_version'];
+
+                // Add changelog entry
+                $changelog->push("- Updated plugin {$updateablePlugins[$plugin]} `{$old_version}` → `{$new_version}`");
+
+                return [$old_version, $new_version];
+            }, "Updating {$updateablePlugins[$plugin]}");
+
+            if ($versions === null) {
+                warning("Update might have failed: {$pluginupdate[0]['status']}");
                 continue;
             }
 
-            // Skip plugins that don't have an update available
-            if ($plugin['update'] !== 'available') {
-                $io->info("Skipping {$plugin['name']}");
-                continue;
-            }
+            list($old_version, $new_version) = $versions;
 
-            // Ask if update should be installed
-            if (!$io->confirm("Update available for {$plugin['name']}. Install?", true)) {
-                $io->info("Skipping {$plugin['name']}");
-                continue;
-            }
-
-            // Get more info
-            $plugininfo = null;
-            exec("wp plugin get {$plugin['name']} --format=json", $plugininfo);
-            $plugininfo = json_decode($plugininfo[0], true);
-
-            // Update the plugin
-            $pluginupdate = null;
-            exec("wp plugin update {$plugin['name']} --format=json", $pluginupdate);
-            $pluginupdate = json_decode($pluginupdate[0], true);
-
-            // Check for the status
-            if ($pluginupdate[0]['status'] !== "Updated") {
-                $io->warning("Update might have failed: {$pluginupdate[0]['status']}");
-                continue;
-            }
-
-            $old_version = $pluginupdate[0]['old_version'];
-            $new_version = $pluginupdate[0]['new_version'];
-
-            // Add changelog entry
-            $changelog->push("- Updated plugin {$plugininfo['title']} `{$old_version}` → `{$new_version}`");
-
-            $io->success("Update installed {$old_version} → {$new_version}");
+            note("Update for {$updateablePlugins[$plugin]} installed {$old_version} → {$new_version}");
 
             // Git commit
-            if ($io->confirm("Commit to git?", true)) {
-                exec("git add -A");
-                exec("git commit -m\"update {$plugin['name']}\"");
+            if ($autoCommit || confirm("Commit changes to git?")) {
+                spin(function () use ($plugin) {
+                    exec("git add -A");
+                    exec("git commit -m\"update {$plugin}\"");
+                }, "Committing changes to git");
             }
         }
 
         // Update translations
-        if ($io->confirm("Update translations?", true)) {
-            exec("wp language core update");
-            exec("wp language plugin update --all");
+        if (confirm("Update translations?")) {
+            spin(function () {
+                exec("wp language core update");
+                exec("wp language plugin update --all");
+            }, "Updating translations");
 
             if (!$this->wdIsClean()) {
-                $io->success("Updated translations");
+                note("Updated translations");
 
                 $changelog->push("- Updated translations");
 
                 // Git commit
-                if ($io->confirm("Commit to git?", true)) {
+                if (confirm("Commit changes to git?")) {
                     exec("git add -A");
                     exec("git commit -m\"update translations\"");
                 }
             } else {
-                $io->success("Translations already up to date");
+                note("Translations already up to date");
             }
         }
 
         // Push to remove and create a release?
-        if ($io->confirm("Push to remote?", true)) {
+        if (confirm("Push to remote?")) {
             exec("git push");
-            $io->success("Pushed to remote");
+            note("Pushed to remote");
 
-            if ($io->confirm("Create release?", true)) {
+            if (confirm("Create release?")) {
                 // Create temporary file with changelog
                 $temp = tempnam(sys_get_temp_dir(), 'changelog');
                 file_put_contents($temp, $changelog->implode("\n"));
@@ -159,7 +190,7 @@ class UpdateCommand extends Command
                 // Remove temporary file
                 unlink($temp);
 
-                $io->success("Created release {$date}");
+                note("Created release {$date}");
 
                 // Open releases in browser
                 exec("gh browse --releases");
@@ -167,6 +198,7 @@ class UpdateCommand extends Command
         }
 
         // Output changelog
+        $io = new SymfonyStyle($input, $output);
         $io->title("Changelog");
         $io->writeln($changelog->toArray());
         $io->newLine();
